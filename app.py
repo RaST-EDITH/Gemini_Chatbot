4878a806e51f1819ee37d17bfc59f640f6a1270f
NAVENDRA SINGH <raghvsinghtomar1975@gmail.com>
2024-04-24 23:00:17 +0530

    Library setup

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 # Chatbot using Gemini API
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+import streamlit as st
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
