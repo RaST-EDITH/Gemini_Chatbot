1460d138c9953f96a732fbd82e41f1d4a4203914
NAVENDRA SINGH <raghvsinghtomar1975@gmail.com>
2024-05-08 21:15:14 +0530

    Gradio base Geminni bot

---
+++ gradio_application.py
@@ -0,0 +1,19 @@
+# Chatbot using Gemini API
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+import gradio
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# Funtion to load Gemini Pro model and get response
+
+model = genai.GenerativeModel("gemini-pro")
+chat = model.start_chat(history=[])
+
+def get_gemini_response(question) :
+
+    response = chat.send_message( question, stream=True)
+    return response
